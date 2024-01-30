2664077362f5bd71e8f785eb58072f029c6d18fc
KiYo <kiyonori.nakashima@gmail.com>
2024-01-30 17:41:56 +0900

    add converter-gson and gson dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,9 +36,13 @@ android {
 }
 
 dependencies {
+    val retrofit_version = "2.9.0"
 
     //Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofit_version")
+    //Gson
+    implementation("com.google.code.gson:gson:2.10.1")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
