5e568ab7fe23fdce3c50f56c298c9533ea6cecb2
KiYo <kiyonori.nakashima@gmail.com>
2024-03-10 14:33:06 +0900

    add getPost() and getUser() on JSONPlaceholderService

--- app/src/main/java/com/example/jsonplaceholder/network/JSONPlaceholderService.kt
+++ app/src/main/java/com/example/jsonplaceholder/network/JSONPlaceholderService.kt
@@ -2,6 +2,7 @@ package com.example.jsonplaceholder.network
 
 import com.example.jsonplaceholder.model.AlbumModel
 import com.example.jsonplaceholder.model.PostModel
+import com.example.jsonplaceholder.model.UserModel
 import retrofit2.Response
 import retrofit2.http.GET
 
@@ -9,6 +10,12 @@ interface JSONPlaceholderService {
     @GET("posts")
     suspend fun getPosts(): Response<List<PostModel>>
 
+    @GET("posts/{id}")
+    suspend fun getPost(id: Int): Response<PostModel>
+
     @GET("albums")
-    suspend fun getAlbums():Response<List<AlbumModel>>
+    suspend fun getAlbums(): Response<List<AlbumModel>>
+
+    @GET("user/{id}")
+    suspend fun getUser(id: Int): Response<UserModel>
 }
