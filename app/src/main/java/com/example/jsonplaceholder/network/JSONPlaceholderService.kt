93f70d09be1b75c220321e580d1b00700b5dd282
KiYo <kiyonori.nakashima@gmail.com>
2024-03-12 21:03:07 +0900

    fix JSONPlaceholderService methods

--- app/src/main/java/com/example/jsonplaceholder/network/JSONPlaceholderService.kt
+++ app/src/main/java/com/example/jsonplaceholder/network/JSONPlaceholderService.kt
@@ -6,20 +6,22 @@ import com.example.jsonplaceholder.model.PostModel
 import com.example.jsonplaceholder.model.UserModel
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface JSONPlaceholderService {
     @GET("posts")
     suspend fun getPosts(): Response<List<PostModel>>
 
     @GET("posts/{id}")
-    suspend fun getPost(id: Int): Response<PostModel>
+    suspend fun getPost(@Path("id") id: Int): Response<PostModel>
 
-    @GET("comments?postId={postId}")
-    suspend fun getCommentsOnPost(postId: Int): Response<List<CommentModel>>
+    @GET("comments")
+    suspend fun getCommentsOnPost(@Query("postId") postId: Int): Response<List<CommentModel>>
 
     @GET("albums")
     suspend fun getAlbums(): Response<List<AlbumModel>>
 
-    @GET("user/{id}")
-    suspend fun getUser(id: Int): Response<UserModel>
+    @GET("users/{id}")
+    suspend fun getUser(@Path("id") id: Int): Response<UserModel>
 }
