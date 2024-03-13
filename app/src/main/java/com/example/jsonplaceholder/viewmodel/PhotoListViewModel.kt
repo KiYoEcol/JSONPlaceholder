dcd44a72faf8beca7d0549477d76ca5f8724a9f0
KiYo <kiyonori.nakashima@gmail.com>
2024-03-13 21:26:14 +0900

    Add PhotoListViewModel.kt

---
+++ app/src/main/java/com/example/jsonplaceholder/viewmodel/PhotoListViewModel.kt
@@ -0,0 +1,64 @@
+package com.example.jsonplaceholder.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.jsonplaceholder.model.PhotoModel
+import com.example.jsonplaceholder.model.UserModel
+import com.example.jsonplaceholder.network.Future
+import com.example.jsonplaceholder.repository.JSONPlaceholderRepository
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+class PhotoListViewModel : ViewModel() {
+    private val repository = JSONPlaceholderRepository()
+    private val _user = MutableLiveData<UserModel>()
+    val user: LiveData<UserModel> = _user
+    private val _isProceedingUser = MutableLiveData<Boolean>()
+    val isProceedingUser: LiveData<Boolean> = _isProceedingUser
+    private val _photos = MutableLiveData<List<PhotoModel>>()
+    val photos: LiveData<List<PhotoModel>> = _photos
+    private val _isProceedingPhotos = MutableLiveData<Boolean>()
+    val isProceedingPhotos: LiveData<Boolean> = _isProceedingPhotos
+    private val _showErrorMessage = MutableLiveData<String>()
+    val showErrorMessage: LiveData<String> = _showErrorMessage
+
+    fun getUser(userId: Int) {
+        viewModelScope.launch {
+            repository.getUserFlow(userId).collectLatest {
+                when (it) {
+                    is Future.Proceeding -> _isProceedingUser.postValue(true)
+                    is Future.Success -> {
+                        _isProceedingUser.postValue(false)
+                        _user.postValue(it.value)
+                    }
+
+                    is Future.Error -> {
+                        _isProceedingUser.postValue(false)
+                        _showErrorMessage.postValue(it.error.message)
+                    }
+                }
+            }
+        }
+    }
+
+    fun getPhotosOnAlbum(albumId: Int) {
+        viewModelScope.launch {
+            repository.getPhotosOnAlbum(albumId).collectLatest {
+                when (it) {
+                    is Future.Proceeding -> _isProceedingPhotos.postValue(true)
+                    is Future.Success -> {
+                        _isProceedingPhotos.postValue(false)
+                        _photos.postValue(it.value)
+                    }
+
+                    is Future.Error -> {
+                        _isProceedingPhotos.postValue(false)
+                        _showErrorMessage.postValue(it.error.message)
+                    }
+                }
+            }
+        }
+    }
+}
