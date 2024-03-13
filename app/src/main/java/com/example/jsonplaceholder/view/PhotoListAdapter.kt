a07f526bf707c19015e1c1d25c578ffb32815de9
KiYo <kiyonori.nakashima@gmail.com>
2024-03-13 21:24:33 +0900

    Add PhotoListAdapter.kt

---
+++ app/src/main/java/com/example/jsonplaceholder/view/PhotoListAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.jsonplaceholder.view
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.lifecycle.LifecycleOwner
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.jsonplaceholder.databinding.ItemPhotoBinding
+import com.example.jsonplaceholder.model.PhotoModel
+
+private object PhotoDiffCallback : DiffUtil.ItemCallback<PhotoModel>() {
+    override fun areItemsTheSame(oldItem: PhotoModel, newItem: PhotoModel): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: PhotoModel, newItem: PhotoModel): Boolean {
+        return oldItem == newItem
+    }
+}
+
+class PhotoListAdapter(private val lifecycleOwner: LifecycleOwner) :
+    ListAdapter<PhotoModel, PhotoListAdapter.PhotoListViewHolder>(PhotoDiffCallback) {
+    class PhotoListViewHolder(private val binding: ItemPhotoBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(lifecycleOwner: LifecycleOwner, photo: PhotoModel) {
+            binding.lifecycleOwner = lifecycleOwner
+            binding.photo = photo
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoListViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemPhotoBinding.inflate(inflater, parent, false)
+        return PhotoListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: PhotoListViewHolder, position: Int) {
+        holder.bind(lifecycleOwner, getItem(position))
+    }
+}
