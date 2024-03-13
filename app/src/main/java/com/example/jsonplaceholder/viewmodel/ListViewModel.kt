8f5218c5f71f9bd609a773bfd11251bd2e3824ed
KiYo <kiyonori.nakashima@gmail.com>
2024-03-13 18:17:39 +0900

    Add ListViewModel.kt

---
+++ app/src/main/java/com/example/jsonplaceholder/viewmodel/ListViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.jsonplaceholder.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+abstract class ListViewModel: ViewModel() {
+    protected val _isProceeding = MutableLiveData<Boolean>(true)
+    val isProceeding: LiveData<Boolean> = _isProceeding
+}
