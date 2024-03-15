385a18d5247c2a72fb4fd3678ad3832429c0a7fe
KiYo <kiyonori.nakashima@gmail.com>
2024-03-15 21:26:54 +0900

    Add UserViewPagerAdapter.kt

---
+++ app/src/main/java/com/example/jsonplaceholder/view/UserViewPagerAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.jsonplaceholder.view
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class UserViewPagerAdapter(fragment: UserFragment, private val userId: Int) :
+    FragmentStateAdapter(fragment) {
+    override fun getItemCount(): Int {
+        return 4
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        val args = Bundle().apply {
+            putInt("userId", userId)
+        }
+        return when (position) {
+            0 -> UserDetailFragment().apply {
+                arguments = args
+            }
+
+            1 -> PostListFragment().apply {
+                arguments = args
+            }
+
+            2 -> AlbumListFragment().apply {
+                arguments = args
+            }
+
+            else -> TodoListFragment().apply {
+                arguments = args
+            }
+        }
+    }
+}
