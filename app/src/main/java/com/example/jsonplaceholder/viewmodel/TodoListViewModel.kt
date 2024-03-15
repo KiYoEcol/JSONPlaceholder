3243dc98450d31fc3383283370649188d858ac1c
KiYo <kiyonori.nakashima@gmail.com>
2024-03-15 21:16:53 +0900

    Add TodoListViewModel.kt

---
+++ app/src/main/java/com/example/jsonplaceholder/viewmodel/TodoListViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.jsonplaceholder.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.jsonplaceholder.model.TodoModel
+import com.example.jsonplaceholder.network.Future
+import com.example.jsonplaceholder.repository.JSONPlaceholderRepository
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+class TodoListViewModel : ListViewModel() {
+    private val repository = JSONPlaceholderRepository()
+    private val _todos = MutableLiveData<List<TodoModel>>()
+    val todos: LiveData<List<TodoModel>> = _todos
+
+    fun getTodosOnUser(userId: Int) {
+        viewModelScope.launch {
+            repository.getTodosOnUser(userId).collectLatest {
+                when (it) {
+                    is Future.Proceeding -> _isProceeding.postValue(true)
+                    is Future.Success -> {
+                        _isProceeding.postValue(false)
+                        _todos.postValue(it.value)
+                    }
+
+                    is Future.Error -> {
+                        _isProceeding.postValue(false)
+                        _showErrorMessage.postValue(it.error.message)
+                    }
+                }
+            }
+        }
+    }
+}
