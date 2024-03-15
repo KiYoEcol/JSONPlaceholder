8905b7a21fc9231be317d054af4adff87562f473
KiYo <kiyonori.nakashima@gmail.com>
2024-03-15 21:28:52 +0900

    add tint_res for BindingAdapter

--- app/src/main/java/com/example/jsonplaceholder/view/BindingAdapter.kt
+++ app/src/main/java/com/example/jsonplaceholder/view/BindingAdapter.kt
@@ -1,15 +1,24 @@
 package com.example.jsonplaceholder.view
 
 import android.widget.ImageView
+import androidx.annotation.ColorInt
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
 object BindingAdapter {
     @BindingAdapter("load_image_url")
     @JvmStatic
-    fun loadImageUrl(imageView: ImageView, url: String?){
+    fun loadImageUrl(imageView: ImageView, url: String?) {
         Glide.with(imageView)
             .load(url)
             .into(imageView)
+    }
+
+    @BindingAdapter("tint_res")
+    @JvmStatic
+    fun tintResId(imageView: ImageView, @ColorInt resId: Int?) {
+        resId?.let {
+            imageView.setColorFilter(it)
+        }
     }
 }
