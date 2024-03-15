cfd572fe8e063eca722de27c65d509a3681f3479
KiYo <kiyonori.nakashima@gmail.com>
2024-03-15 21:27:25 +0900

    Add UserViewModel.kt

---
+++ app/src/main/java/com/example/jsonplaceholder/viewmodel/UserViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.jsonplaceholder.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.jsonplaceholder.model.UserModel
+import com.example.jsonplaceholder.network.Future
+import com.example.jsonplaceholder.repository.JSONPlaceholderRepository
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+class UserViewModel : ViewModel() {
+    private val repository = JSONPlaceholderRepository()
+    private val _user = MutableLiveData<UserModel>()
+    val user: LiveData<UserModel> = _user
+    private val _isProceeding = MutableLiveData<Boolean>()
+    val isProceeding: LiveData<Boolean> = _isProceeding
+    private val _showErrorMessage = MutableLiveData<String>()
+    val showErrorMessage: LiveData<String> = _showErrorMessage
+
+    fun getUser(userId: Int) {
+        viewModelScope.launch {
+            repository.getUserFlow(userId).collectLatest {
+                when (it) {
+                    is Future.Proceeding -> _isProceeding.postValue(true)
+                    is Future.Success -> {
+                        _isProceeding.postValue(false)
+                        _user.postValue(it.value)
+                    }
+
+                    is Future.Error -> {
+                        _isProceeding.postValue(false)
+                        _showErrorMessage.postValue(it.error.message)
+                    }
+                }
+            }
+        }
+    }
+}
