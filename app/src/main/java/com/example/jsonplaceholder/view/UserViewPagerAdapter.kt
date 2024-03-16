4d6fdb6ad913ff8446b375d5ebec3a7a92dfa6ce
KiYo <kiyonori.nakashima@gmail.com>
2024-03-16 18:20:25 +0900

    apply dark mode

--- app/src/main/java/com/example/jsonplaceholder/view/UserViewPagerAdapter.kt
+++ app/src/main/java/com/example/jsonplaceholder/view/UserViewPagerAdapter.kt
@@ -1,7 +1,6 @@
 package com.example.jsonplaceholder.view
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
